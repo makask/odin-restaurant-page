92474f071d3fc67026e7054a36952c252479deb4
Marko Kask <kaskmarko@gmail.com>
2023-05-23 20:42:25 +0300

    Add some dummy data

--- src/contact.js
+++ src/contact.js
@@ -2,11 +2,63 @@ function createContact() {
   const contact = document.createElement('div');
   contact.classList.add('contact');
   contact.classList.add('main-item');
-  const text = document.createElement('h1');
-  text.textContent = 'Contact';
-  contact.appendChild(text);
+
+  contact.appendChild(
+    createContactInfo(
+      '123 323 345',
+      'Suur-Karja 8, Tallinn',
+      'images/address.png'
+    )
+  );
+
   const main = document.querySelector('.main');
   main.appendChild(contact);
+}
+
+function createContactInfo(phoneNumber, address, imageSrc) {
+  const contact = document.createElement('div');
+  contact.classList.add('contact-wrapper');
+
+  const phoneInfo = document.createElement('div');
+  phoneInfo.classList.add('phone-info');
+
+  const phoneIcon = document.createElement('img');
+  phoneIcon.classList.add('icon');
+  phoneIcon.src = 'images/phone.svg';
+
+  const telephone = document.createElement('p');
+  telephone.classList.add('phone');
+  telephone.textContent = phoneNumber;
+
+  phoneInfo.appendChild(phoneIcon);
+  phoneInfo.appendChild(telephone);
+
+  const addressInfo = document.createElement('div');
+  addressInfo.classList.add('address-info');
+
+  const homeIcon = document.createElement('img');
+  homeIcon.classList.add('icon');
+  homeIcon.src = 'images/home.svg';
+
+  const contactAddress = document.createElement('p');
+  contactAddress.classList.add('address');
+  contactAddress.textContent = address;
+
+  addressInfo.appendChild(homeIcon);
+  addressInfo.appendChild(contactAddress);
+
+  contact.appendChild(phoneInfo);
+  contact.appendChild(addressInfo);
+  contact.appendChild(addContactPicture(imageSrc));
+
+  return contact;
+}
+
+function addContactPicture(imageSrc) {
+  const contactImage = document.createElement('img');
+  contactImage.classList.add('contact-img');
+  contactImage.src = imageSrc;
+  return contactImage;
 }
 
 export default createContact;
--- src/menu.js
+++ src/menu.js
@@ -2,11 +2,71 @@ function createMenu() {
   const menu = document.createElement('div');
   menu.classList.add('menu');
   menu.classList.add('main-item');
-  const text = document.createElement('h1');
-  text.textContent = 'MENU';
-  menu.appendChild(text);
+
+  menu.appendChild(
+    addMenuItem(
+      'Pulled pork',
+      'Low and slow pulled pork, smoked with local apple wood',
+      'images/pulledpork.png'
+    )
+  );
+
+  menu.appendChild(
+    addMenuItem(
+      'Pulled pork',
+      'Low and slow pulled pork, smoked with local apple wood',
+      'images/pulledpork.png'
+    )
+  );
+
+  menu.appendChild(
+    addMenuItem(
+      'Pulled pork',
+      'Low and slow pulled pork, smoked with local apple wood',
+      'images/pulledpork.png'
+    )
+  );
+
+  menu.appendChild(
+    addMenuItem(
+      'Pulled pork',
+      'Low and slow pulled pork, smoked with local apple wood',
+      'images/pulledpork.png'
+    )
+  );
+
+  menu.appendChild(
+    addMenuItem(
+      'Pulled pork',
+      'Low and slow pulled pork, smoked with local apple wood',
+      'images/pulledpork.png'
+    )
+  );
+
   const main = document.querySelector('.main');
   main.appendChild(menu);
+}
+
+function addMenuItem(name, description, imageSource) {
+  const item = document.createElement('div');
+  item.classList.add('menu-item');
+  item.appendChild(addMenuItemImage(imageSource));
+  const itemName = document.createElement('p');
+  itemName.classList.add('menu-item-name');
+  itemName.textContent = name;
+  const itemDescription = document.createElement('p');
+  itemDescription.classList.add('menu-item-description');
+  itemDescription.textContent = description;
+  item.appendChild(itemName);
+  item.appendChild(itemDescription);
+  return item;
+}
+
+function addMenuItemImage(imageSrc) {
+  const itemImage = document.createElement('img');
+  itemImage.classList.add('menu-item-img');
+  itemImage.src = imageSrc;
+  return itemImage;
 }
 
 export default createMenu;
